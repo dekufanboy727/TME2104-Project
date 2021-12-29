44130d5c67d1a97fa4c32f6d5bb5fd930c4a5b87
C-YunSin2000 <ys_chong00@hotmail.com>
2021-12-29 21:53:34 +0800

    checkout (not done yet
    checkout (not done yet

---
+++ checkout.php
@@ -0,0 +1,91 @@
+<?php session_start(); ?>
+<!DOCTYPE html>
+
+<html lang="en">
+    <head>
+        <title>Cart - PNWX</title>
+        <link rel="icon" href="Pictures/icon.png">
+        <link rel="shortcut icon" href="Pictures/icon.png">
+        <link rel="stylesheet" href="CSS/checkout.css">
+    </head>
+
+    <body>
+        <?php 
+            //session login NOT set MEAN user is NOT logged in
+            //PUBLIC user
+            if (!isset($_SESSION['login'])) 
+            {
+                //Redirecting public user to registration page upon clicking check out button
+                header("Location: register.php"); 
+            }
+
+            
+        ?>
+
+        <header class="HeaderHeader">
+            <a href="index.php"> <img class="logo" src="Pictures/LOGO.jpeg" alt="Pacific Northwest X-Ray Inc."> </a>
+            <span class = "menu">
+                <h3>Checking Out - Payment</h3>
+                <a class = "cart_position" href="cart.php"> <img id="cart" src="Pictures/cart.png" alt="Cart"> </a>
+            </span>
+            <div class="login_register">
+                <ul>
+                  <li><a href="logout.php"  >Logout</a></li>
+                  <li>| </li>
+                  <li><a href="myProfile.php"  >My Profile</a></li>
+                  <li><img class="image_login_register" src="Pictures/login_register_icon.png" alt="Login and register icon"></a>
+                </ul>
+            </div>
+        </header>
+
+
+    <footer class="FooterFooter">
+                <div class="FFooterUpperPortion">
+                    <div class="FFooterIcon">
+                        <img src="Pictures/LOGO.jpeg" alt="Pacific Northwest X-Ray Inc.">
+                    </div>
+                    <div class="FFooterBlocks">
+                        <h3><b>Contact Us</b></h3>
+                        <ul>
+                            <li>Tel: 503-667-3000</li>
+                            <br>
+                            <li>Toll-Free: 800-827-9729</li>
+                            <br>
+                            <li>Fax: 503-666-8855</li>
+                        </ul>
+                    </div>
+                    <div class="FFooterBlocks">
+                        <h3><b>Reach Us</b></h3>
+                        <ul>
+                            <li>P.O. Box 625,</li>
+                            <br>
+                            <li>Gresham, OR</li>
+                            <br>
+                            <li>97030 U.S.A.</li>
+                        </ul>
+                    </div>
+                    <div class="FFooterBlocks">
+                        <h3><b>Opening Hours</b></h3>
+                        <ul>
+                            <li>8am - 5pm</li>
+                            <br>
+                            <li>Monday-Friday</li>
+                            <br>
+                            <li>(PST/PT)</li>
+                        </ul>
+                    </div>
+                </div>
+
+                <br>
+                <br>
+                <hr id="FooterLine"/>
+                <div class="FFooterLowerPortion" >
+                <sub class="Disclaimer">This web site is our catalog! <u>No printed catalog is available.</u></sub>
+                <br>
+                <sub class="Disclaimer">©1997-2021 Pacific Northwest X-Ray Inc. - Sales & Marketing Division - All Rights Reserved</sub>
+                </div>
+            </footer>
+
+    </body>
+
+</html>
