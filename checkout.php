8cc3a7694c1a7b09d510404a102155cca19c654e
C-YunSin2000 <ys_chong00@hotmail.com>
2021-12-29 23:17:59 +0800

    not done yet

--- checkout.php
+++ checkout.php
@@ -10,12 +10,6 @@
     </head>
 
     <body>
-        <?php 
-   
-
-            
-        ?>
-
         <header class="HeaderHeader">
             <a href="index.php"> <img class="logo" src="Pictures/LOGO.jpeg" alt="Pacific Northwest X-Ray Inc."> </a>
             <span class = "menu">
@@ -31,9 +25,93 @@
                 </ul>
             </div>
         </header>
+        
+        <main>
+            <?php 
+                include 'connection.php';
+                $userid = $_SESSION['user_id'];
+                $cartid = $_SESSION['cartid'];
 
+                //Select user info from the table
+                $sql = "SELECT firstname, lastname, region, phone, _state, postcode, _address, city 
+                FROM registered_user WHERE id='$userid'";
+                $result = mysqli_query($conn,$sql); 
+                $user_making_payment = mysqli_fetch_assoc($result); //Fetch the selected info
+                
+            ?>
+            <img src="Pictures/delivery.png">
+            <p>Delivery Address</p> <br>
+            <?php
+                echo $user_making_payment['lastname']." ".$user_making_payment['firstname'];
+                echo "+".$user_making_payment['region']." ".$user_making_payment['phone'];
+                echo $user_making_payment['_address'].", ".$user_making_payment['city'].", ".$user_making_payment['postcode'].", ".$user_making_payment['_state'].", Malaysia.";
+            ?>
+            <br>
 
-    <footer class="FooterFooter">
+            <?php
+                $total_quantity = $shippingfee = $paymentotal = 0;
+                $sql = "SELECT Cart_Item.Cart_id, product.id, product.productname, Cart_Item.Quantity, product.price, Cart_Item.Subtotal 
+                FROM Cart_Item
+                INNER JOIN product
+                ON Cart_Item.Product_id = product.id /*Find the same PRODUCT ID*/
+                WHERE Cart_Item.Cart_id = '$cartid'"; /*Locate that person's CART ID*/
+
+                $result = mysqli_query ($conn,$sql);
+
+                if(mysqli_num_rows($result) > 0)
+                {
+                    $total_quantity = mysqli_num_rows($result); //Fetch the total number of items
+                    while($row = mysqli_fetch_assoc($result))
+                    {
+                        echo'<tr align = "center">';
+                        echo '<td >'.$row['productname']. '</td><br>';
+                        echo '<td>'.$row['Quantity'].'</td><br>';
+                        echo '<td>' .$row['price']. '</td><br>';
+                        echo '<td>' .$row['Subtotal']. '</td><br>';
+                        echo'</tr>';
+                    }
+                }
+
+                if($total_quantity <= 5 )
+                {
+                    $shippingfee = 55.50;
+                }
+                else if($total_quantity > 5 && $total_quantity <= 10)
+                {
+                    $shippingfee = 100.50;
+                }
+                else if($total_quantity > 10 && $total_quantity <= 15)
+                {
+                    $shippingfee = 150.50;
+                }
+                else if($total_quantity > 15 && $total_quantity <= 20)
+                {
+                    $shippingfee = 210.50;
+                }
+                else if($total_quantity > 15 && $total_quantity <= 20)
+                {
+                    $shippingfee = 270.50;
+                }
+                else
+                {
+                    $shippingfee = 340.50;
+                }
+
+                $sql = "SELECT Grand_total FROM ShoppingCart WHERE id = '$cartid'"; 
+                $result = mysqli_query ($conn,$sql);
+                $user_making_payment = mysqli_fetch_assoc($result);
+
+                $paymentotal = $user_making_payment['Grand_total'] + $shippingfee;
+                echo "Merchandise Subtotal: ".$user_making_payment['Grand_total'];
+                echo "Shipping Subtotal: ".$shippingfee;
+                echo "Payment Total (".$total_quantity." Item): ".$paymentotal;
+            ?>
+
+        
+
+        </main>
+
+        <footer class="FooterFooter">
                 <div class="FFooterUpperPortion">
                     <div class="FFooterIcon">
                         <img src="Pictures/LOGO.jpeg" alt="Pacific Northwest X-Ray Inc.">
